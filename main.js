ce1b96db367b2cf50f3240021b6eba398bcbcb45
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-03-22 12:23:11 -0400

    First step ... working if conditional but need to tweek the loops and logic to match requirements

--- main.js
+++ main.js
@@ -1,50 +1,116 @@
-const btn1 = document.querySelector('button');
+const btn = document.querySelector('.nameBox');
+
+btn.addEventListener('click', attack) //on click event we attack
 
 class MySpaceShip {
-    constructor()
-    {
+    constructor() {
         this.hull = 20;
         this.firePower = 5;
         this.accuracy = 0.7;
     }
+
+    getHull() {
+        return this.hull;
+    }
+
+    getFirepower() {
+        return this.firePower;
+    }
+
+    getAccuracy() {
+        return this.accuracy;
+    }
+
+    displayStats()
+    {
+        console.log(`Hero current stats:\nHull: ${this.hull}\n\nFirepower: ${this.firePower}\n\nAccuracy: ${this.accuracy}`);
+    }
+
+    decreaseHealth(damage) {
+        this.hull -= damage;
+    }
+
 }
 
 class AlienShip {
-    constructor()
-    {
+    constructor() {
         this.hull = Math.floor(Math.random() * (6 - 3)) + 3;
         this.firePower = Math.floor(Math.random() * (4 - 2)) + 2;
         this.accuracy = Math.floor(Math.random() * (0.8 - 0.6)) + 0.6;
     }
 
     //getters
-    getHull()
-    {
+    getHull() {
         return this.hull;
     }
 
-    getFirepower()
-    {
+    getFirepower() {
         return this.firePower;
     }
 
-    getAccuracy()
-    {
+    getAccuracy() {
         return this.accuracy;
     }
-}
 
-class AlienShipFactory extends AlienShip{
-    constructor()
+    displayStats()
     {
-        super();
+        console.log(`Enemy current stats:\nHull: ${this.hull}\n\nFirepower: ${this.firePower}\n\nAccuracy: ${this.accuracy}`);
     }
 
-    generateAlienShip()
-    {
-        let alienShip = new AlienShip();
+    decreaseHealth(damage) {
+        this.hull -= damage;
     }
 }
 
-let test = new AlienShipFactory();
-console.log()
+// class AlienShipFactory {
+
+//     generateAlienShip()
+//     {
+//         let alienShip = new AlienShip();
+//     }
+// }
+
+
+
+//function attack
+function attack() {
+    let hero = new MySpaceShip();
+    let enemy = new AlienShip();
+
+   //display initial stats
+   hero.displayStats();
+   enemy.displayStats();
+
+    //the moment we attack, decrease enemy health by the ship's firepower
+    enemy.decreaseHealth(hero.getFirepower());
+
+
+    if(enemy.getHull != 0 && Math.random() < enemy.getAccuracy())
+    {
+        console.log('You have been hit!');
+        hero.decreaseHealth(enemy.firePower());
+    }
+
+   if(hero.getHull() > 0 && enemy.getHull() != 0)
+   {
+    while (hero.getHull() > 0) {
+        enemy.decreaseHealth(hero.getFirepower()); //attack enemy ship
+        console.log(`Enemy stats:\n${enemy.displayStats()}`);
+
+        if (enemy.getHull() != 0 && Math.random() < enemy.getAccuracy()) //if enemy ship survives
+        {
+
+            hero.decreaseHealth(enemy.getFirepower());
+            console.log(`Hero stats:\n${hero.displayStats()}`);
+        } else {
+            console.log('Enemy is weak. Attack!')
+            enemy.decreaseHealth(hero.getFirepower());
+            console.log(`Enemy stats:\n${enemy.displayStats()}`);
+        }
+    }
+
+   }
+
+
+
+}
