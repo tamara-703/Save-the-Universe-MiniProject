2a82712607842c6019fb3b013f04aa13361b544f
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-03-22 12:47:30 -0400

    Still refining conditionals. Closer than first step

--- main.js
+++ main.js
@@ -21,8 +21,7 @@ class MySpaceShip {
         return this.accuracy;
     }
 
-    displayStats()
-    {
+    displayStats() {
         console.log(`Hero current stats:\nHull: ${this.hull}\n\nFirepower: ${this.firePower}\n\nAccuracy: ${this.accuracy}`);
     }
 
@@ -34,7 +33,7 @@ class MySpaceShip {
 
 class AlienShip {
     constructor() {
-        this.hull = Math.floor(Math.random() * (6 - 3)) + 3;
+        this.hull = Math.floor(Math.random() * (20 - 3)) + 3;
         this.firePower = Math.floor(Math.random() * (4 - 2)) + 2;
         this.accuracy = Math.floor(Math.random() * (0.8 - 0.6)) + 0.6;
     }
@@ -52,8 +51,7 @@ class AlienShip {
         return this.accuracy;
     }
 
-    displayStats()
-    {
+    displayStats() {
         console.log(`Enemy current stats:\nHull: ${this.hull}\n\nFirepower: ${this.firePower}\n\nAccuracy: ${this.accuracy}`);
     }
 
@@ -77,40 +75,32 @@ function attack() {
     let hero = new MySpaceShip();
     let enemy = new AlienShip();
 
-   //display initial stats
-   hero.displayStats();
-   enemy.displayStats();
+    //display initial stats
+    hero.displayStats();
+    enemy.displayStats();
 
-    //the moment we attack, decrease enemy health by the ship's firepower
     enemy.decreaseHealth(hero.getFirepower());
 
-
-    if(enemy.getHull != 0 && Math.random() < enemy.getAccuracy())
-    {
+    if (enemy.getHull() > 0 && Math.random() < enemy.getAccuracy()) {
         console.log('You have been hit!');
-        hero.decreaseHealth(enemy.firePower());
-    }
-
-   if(hero.getHull() > 0 && enemy.getHull() != 0)
-   {
-    while (hero.getHull() > 0) {
-        enemy.decreaseHealth(hero.getFirepower()); //attack enemy ship
-        console.log(`Enemy stats:\n${enemy.displayStats()}`);
-
-        if (enemy.getHull() != 0 && Math.random() < enemy.getAccuracy()) //if enemy ship survives
-        {
-
-            hero.decreaseHealth(enemy.getFirepower());
-            console.log(`Hero stats:\n${hero.displayStats()}`);
-        } else {
-            console.log('Enemy is weak. Attack!')
-            enemy.decreaseHealth(hero.getFirepower());
-            console.log(`Enemy stats:\n${enemy.displayStats()}`);
+        hero.decreaseHealth(enemy.getFirepower()); //decrease hero hull based on the enemy's firepower
+        console.log(hero.getHull());
+        console.log(enemy.getHull());
+        if (hero.getHull() > 0) {
+            console.log('Attack!')
+            enemy.decreaseHealth(hero.getFirepower())
+            console.log(hero.getHull());
+            console.log(enemy.getHull());
+            if (enemy.getHull() < 0) {
+                console.log("You won!")
+            } else {
+                console.log("Attack")
+                enemy.decreaseHealth(hero.getFirepower())
+                console.log(hero.getHull());
+                console.log(enemy.getHull());
+            }
         }
     }
-
-   }
-
 
 
 }
