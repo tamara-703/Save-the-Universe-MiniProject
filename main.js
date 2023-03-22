ffffe44ee5de3f1b99ea536e186ca70c0f10861e
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-03-22 14:33:25 -0400

    created all basic classes. Onto the loops and their logic.

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const btn = document.querySelector('.nameBox');
 
 btn.addEventListener('click', attack) //on click event we attack
 
-class MySpaceShip {
-    constructor() {
-        this.hull = 20;
-        this.firePower = 5;
-        this.accuracy = 0.7;
+class Spaceship {
+    constructor(hull, firePower,accuracy)
+    {
+        this.hull = hull;
+        this.firePower = firePower;
+        this.accuracy = accuracy
     }
 
     getHull() {
@@ -21,86 +22,157 @@ class MySpaceShip {
         return this.accuracy;
     }
 
-    displayStats() {
-        console.log(`Hero current stats:\nHull: ${this.hull}\n\nFirepower: ${this.firePower}\n\nAccuracy: ${this.accuracy}`);
-    }
-
-    decreaseHealth(damage) {
+    decreaseHealth(damage)
+    {
         this.hull -= damage;
     }
 
 }
 
-class AlienShip {
+
+class MySpaceShip extends Spaceship{
     constructor() {
-        this.hull = Math.floor(Math.random() * (20 - 3)) + 3;
-        this.firePower = Math.floor(Math.random() * (4 - 2)) + 2;
-        this.accuracy = Math.floor(Math.random() * (0.8 - 0.6)) + 0.6;
+        super(20,5,0.7)
+        this.retreat = false;
     }
 
-    //getters
-    getHull() {
-        return this.hull;
+
+    displayStats() {
+        console.log(`Hero current stats:\nHull: ${super.hull}\n\nFirepower: ${super.firePower}\n\nAccuracy: ${super.accuracy}`);
     }
 
-    getFirepower() {
-        return this.firePower;
+    isRetreat(rt) //create user input here using DOM
+    {
+        if(rt === true)
+        {
+            this.retreat = rt;
+            console.log("Successfully retreated.");
+        } else
+        {
+            console.log("Onto the next fight");
+        }
     }
 
-    getAccuracy() {
-        return this.accuracy;
+}
+
+class AlienShip extends Spaceship {
+
+    constructor() {
+
+        let hull = Math.floor(Math.random() * (20 - 3)) + 3;
+        let firePower = Math.floor(Math.random() * (4 - 2)) + 2;
+        let accuracy = Math.floor(10 * (Math.random() * (0.8 - 0.6) + 0.6))/10;
+
+        super(hull,firePower,accuracy);
+
     }
 
     displayStats() {
-        console.log(`Enemy current stats:\nHull: ${this.hull}\n\nFirepower: ${this.firePower}\n\nAccuracy: ${this.accuracy}`);
-    }
-
-    decreaseHealth(damage) {
-        this.hull -= damage;
+        console.log(`Enemy current stats:\nHull: ${super.hull}\n\nFirepower: ${super.firePower}\n\nAccuracy: ${super.accuracy}`);
     }
 }
 
-// class AlienShipFactory {
+// class AlienShipFactory{
+
+//     constructor()
+//     {
+//         this.shipArray = [];
+//     }
 
 //     generateAlienShip()
 //     {
 //         let alienShip = new AlienShip();
+//         this.shipArray.push(alienShip);
 //     }
+
+//     getHull() {
+//         return this.shipArray[0].hull;
+//     }
+
+//     getFirepower() {
+//         return this.shipArray[0].firePower;
+//     }
+
+//     getAccuracy() {
+//         return this.shipArray[0].accuracy;
+//     }
+
+//     decreaseHealth(damage)
+//     {
+//         if(this.shipArray[0].hull > 0)
+//         {
+//             console.log("Enemy ship attacked");
+//             this.shipArray[0].hull -= damage;
+//             console.log(`Enemy stats: ${this.shipArray[0].hull}`);
+//         } else
+//         {
+//             console.log("Enemy ship is defeated");
+//             console.log(`Enemy stats: ${this.shipArray[0].hull}`);
+//         }
+//     }
+
+//     checkAccuracy()
+//     {
+//         if(Math.random() < this.shipArray[0].accuracy)
+//         {
+//             if(this.shipArray[0].hull > 0)
+//             {
+//                 console.log("You have been hit!");
+//             }
+
+//         }
+//     }
+
 // }
 
 
 
 //function attack
+
+
 function attack() {
-    let hero = new MySpaceShip();
-    let enemy = new AlienShip();
 
-    //display initial stats
-    hero.displayStats();
-    enemy.displayStats();
 
-    enemy.decreaseHealth(hero.getFirepower());
+    for(let i = 0; i < 6; i++)
+    {
+        let hero = new MySpaceShip();
+        let enemy = new AlienShip();
 
-    if (enemy.getHull() > 0 && Math.random() < enemy.getAccuracy()) {
-        console.log('You have been hit!');
-        hero.decreaseHealth(enemy.getFirepower()); //decrease hero hull based on the enemy's firepower
-        console.log(hero.getHull());
-        console.log(enemy.getHull());
-        if (hero.getHull() > 0) {
-            console.log('Attack!')
-            enemy.decreaseHealth(hero.getFirepower())
-            console.log(hero.getHull());
-            console.log(enemy.getHull());
-            if (enemy.getHull() < 0) {
-                console.log("You won!")
-            } else {
-                console.log("Attack")
-                enemy.decreaseHealth(hero.getFirepower())
-                console.log(hero.getHull());
-                console.log(enemy.getHull());
-            }
+        if(hero.getHull() > 0 && enemy.getHull() > 0)
+        {
+            enemy.decreaseHealth(hero.getFirepower());
+            console.log("Attacked enemy. Do you want to retreat?")
+            hero.isRetreat(false);
         }
+
+
+
     }
+
+
+
+    // enemy.decreaseHealth(hero.getFirepower());
+
+    // if (enemy.getHull() > 0 && Math.random() < enemy.getAccuracy()) {
+    //     console.log('You have been hit!');
+    //     hero.decreaseHealth(enemy.getFirepower()); //decrease hero hull based on the enemy's firepower
+    //     console.log(hero.getHull());
+    //     console.log(enemy.getHull());
+    //     if (hero.getHull() > 0) {
+    //         console.log('Attack!')
+    //         enemy.decreaseHealth(hero.getFirepower())
+    //         console.log(hero.getHull());
+    //         console.log(enemy.getHull());
+    //         if (enemy.getHull() < 0) {
+    //             console.log("You won!")
+    //         } else {
+    //             console.log("Attack")
+    //             enemy.decreaseHealth(hero.getFirepower())
+    //             console.log(hero.getHull());
+    //             console.log(enemy.getHull());
+    //         }
+    //     }
+    // }
 
 
 }
